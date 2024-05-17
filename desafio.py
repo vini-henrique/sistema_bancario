c634f7da8d050366c2b003072705526d67edbd81
Vinicius Henrique <134116784+vini-henrique@users.noreply.github.com>
2024-05-17 09:27:18 -0300

    Add files via upload

--- desafio.py
+++ desafio.py
@@ -12,10 +12,10 @@ MENU
 
 
 def verificar_extrato(saldo,/,*,extrato):
-  print("------------EXTRATO------------")
-  print(extrato if extrato else "Nenhuma movimentação realizada")
-  print(f"\nSaldo: R${saldo:.2f}")
-  print("-------------------------------")
+    print("------------EXTRATO------------")
+    print(extrato if extrato else "Nenhuma movimentação realizada")
+    print(f"\nSaldo: R${saldo:.2f}")
+    print("-------------------------------")
 
 def depositar(saldo, extrato,valor,/):
     valor=float(input("Informe o valor do depósito: "))
@@ -47,40 +47,40 @@ def sacar(*,saldo, valor, extrato, numero_saques, limite, LIMITE_SAQUES):
     return saldo, extrato, numero_saques
 
 def cadastrar_usuario(usuarios):
-  cpf = input("Informe o seu CPF(Somente números): ")
-  usuario = validar_usuarios(usuarios, cpf)
-  if usuario:
-    print("Usuário já cadastrado")
-    return
+    cpf = input("Informe o seu CPF(Somente números): ")
+    usuario = validar_usuarios(usuarios, cpf)
+    if usuario:
+        print("Usuário já cadastrado")
+        return
 
-  nome = input("Informe o seu nome: ")
-  data_nascimento = input("Informe a sua data de nascimento (dd-mm-aaaa): ")
-  endereco = input(
-      "Informe o seu endereço (Logradouro, numero-bairro-cidade/sigla do Estado): "
-  )
+    nome = input("Informe o seu nome: ")
+    data_nascimento = input("Informe a sua data de nascimento (dd-mm-aaaa): ")
+    endereco = input(
+        "Informe o seu endereço (Logradouro, numero-bairro-cidade/sigla do Estado): "
+    )
 
-  usuarios.append({
-      "nome": nome,
-      "cpf": cpf,
-      "data_nascimento": data_nascimento,
-      "endereco": endereco
-  })
-  print("Usuário cadastrado com sucesso!")
-  return cpf
+    usuarios.append({
+        "nome": nome,
+        "cpf": cpf,
+        "data_nascimento": data_nascimento,
+        "endereco": endereco
+    })
+    print("Usuário cadastrado com sucesso!")
+    
 
 def validar_usuarios(usuarios, cpf):
-  checar_usuario = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
-  return checar_usuario[0] if checar_usuario else None
+    checar_usuario = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return checar_usuario[0] if checar_usuario else None
 
 
 def criar_conta(agencia, numero_conta, usuarios):
-  cpf = input("Informe o CPF do usuário:")
-  usuario= validar_usuarios(usuarios, cpf)
-  if usuario:
-    print("Conta criada com sucesso!"  )
-    return{"agencia":agencia,"numero_conta": numero_conta, "usuario": usuario}
+    cpf = input("Informe o CPF do usuário:")
+    usuario= validar_usuarios(usuarios, cpf)
+    if usuario:
+        print("Conta criada com sucesso!"  )
+        return{"agencia":agencia,"numero_conta": numero_conta, "usuario": usuario}
 
-  print("Usuário não encontrado, encerrando operação de criação de conta")
+    print("Usuário não encontrado, encerrando operação de criação de conta")
 
 
 
@@ -99,24 +99,26 @@ valor = 0
 contador = 1
 
 while True:
-  opcao=int(input(menu))
-  if opcao == 1:
-    verificar_extrato(saldo, extrato=extrato)
-  elif opcao == 2:
-    saldo,extrato = depositar(saldo, extrato,valor)
-  elif opcao == 3:
-    saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, numero_saques=numero_saques, limite=limite, LIMITE_SAQUES=LIMITE_SAQUES)
-  elif opcao == 4:
-    cadastrar_usuario(usuarios)
-  elif opcao == 5:
-    numero_conta = contador
-    conta = criar_conta(AGENCIA, numero_conta, usuarios)
-    contador+=1
+    opcao=int(input(menu))
+    if opcao == 1:
+        verificar_extrato(saldo, extrato=extrato)
+    elif opcao == 2:
+        saldo,extrato = depositar(saldo, extrato,valor)
+    elif opcao == 3:
+        saldo, extrato, numero_saques = sacar(saldo=saldo, valor=valor, extrato=extrato, numero_saques=numero_saques, limite=limite, LIMITE_SAQUES=LIMITE_SAQUES)
+    elif opcao == 4:
+        cadastrar_usuario(usuarios)
+    elif opcao == 5:
+        numero_conta = contador
+        conta = criar_conta(AGENCIA, numero_conta, usuarios)
+        contador+=1
 
-    if conta:
-      contas.append(conta)
+        if conta:
+            contas.append(conta)
 
-  else:
-    print("Encerrando operações.")
-    break
-
+    elif opcao == 0:
+        print("Encerrando operações.")
+        break
+    else:
+        print("Entrada inválida")
+   
